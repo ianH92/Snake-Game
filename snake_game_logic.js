92f036ed1383d471b9a2d2a01c13de2506b6ce1f
ianH92 <>
2017-12-07 22:33:39 -0800

    basic game functionality complete

--- snake_game_logic.js
+++ snake_game_logic.js
@@ -3,47 +3,28 @@ const numDivisions = 25; // Number of squares each row in the grid contains.
 const minWidth = numDivisions * 4;
 const maxWidth = numDivisions * 40;
 
-// Colors for board and snake
-const col1 = 'red';
-const col2 = 'gold';
-const col3 = 'white';
-const col4 = 'lightgray';
-
 var canvas = document.querySelector('canvas');
 var width = canvas.width;
 var squareSize = width / numDivisions;
 var ctx = canvas.getContext('2d');
-drawGameBoardGrid(ctx, width, squareSize, col3, col4);
 
 var lastKey = 37;
-window.addEventListener('keydown', (event) => { lastKey = event.keyCode; });
+window.addEventListener('keydown', (event) => { 
+	lastKey = (event.keyCode > 36 && event.keyCode < 41) ? event.keyCode : lastKey; 
+});
 
+let snake = new Snake(new Coordinates(12, 12), new Coordinates(12, 6));
+var intervalID = setInterval(function(){ gameLoop(snake, lastKey, ctx, width, squareSize, numDivisions); }, 100);
 
-let snake = new Snake(new SnakeSegment(12, 12, col1, col2));
-let food = new Food();
-food.createNewFood(numDivisions, col1, col2, snake);
-//setInterval(function(){ gameLoop(snake, food, lastKey, ctx, width, squareSize, col1, col2, col3, col4); }, 100);
-
-
-/* TESTED
-*/
-function Food() {
-	this.food = null
+function gameOver(ctx, width) {
+	ctx.fillStyle = 'white';
+	ctx.fillRect(0, 0, width, width);
 	
-	this.getFood = function() { return this.food; }
-	this.createNewFood = function(numDivisions, col1, col2, snake) {
-		let x = getRand(numDivisions);
-		let y = getRand(numDivisions);
-		
-		while(snake.intersectsSnake(x, y)) {
-			x = getRand(numDivisions);
-			y = getRand(numDivisions);
-		}
-		this.food = new SnakeSegment(x, y, col1, col2);
-	}
+	ctx.font = '55px monospace';
+	ctx.fillStyle = 'red';
+	ctx.fillText('GAME OVER', width / 5 , width / 2);
+	clearInterval(intervalID);
 }
-
-
 
 /* TESTED
 */
@@ -51,47 +32,47 @@ function getRand(max) {
 	return Math.floor(Math.random() * max);
 }
 
-
-
-
-
-
-
-function gameLoop(snake, food, lastKey, ctx, width, squareSize, col1, col2, col3, col4) {
-	let newHead = createNextSegment(lastKey, snake.getHeadX(), snake.getHeadY(), col1, col2);
-	let currFood = food.getFood();
-	let grow = (snake.intersectHead(currFood)) ? true : false;
-	if(grow) { food.createNewFood(numDivisions, col1, col2, snake); }
+function gameLoop(snake, lastKey, ctx, width, squareSize, numDivs) {
+	let newHead = createNextSegment(lastKey, snake.getHeadX(), snake.getHeadY());
 	
-	snake.updateSnake(newHead, grow);
-	drawGameBoardGrid(ctx, width, squareSize, col3, col4);
-	drawSnake(ctx, squareSize, snake.getSnake());
-	drawSquare(ctx, squareSize, food.getFood().getX(), food.getFood().getY(), col1, col2);
-}
-
-
-/* TESTED
-*/
-function drawSnake(canvasCtx, squareSize, snake) {
-	for(var i = 0; i < snake.length; i++) {
-		var s = snake[i];
-		drawSquare(canvasCtx, squareSize, s.getX(), s.getY(), s.getColor1(), s.getColor2());
+	if(outsideOfBorders(newHead, numDivs) || snake.intersectsSnake(newHead.getX(), newHead.getY())) {
+		gameOver(ctx, width);
+		return;
 	}
+	let grow = snake.headIntersectsFood();
+	if(grow) { 
+		snake.createNewFood(numDivs); 
+	}
+	snake.updateSnake(newHead, grow);
+	snake.drawSnake(ctx, squareSize);
+	snake.drawFood(ctx, squareSize);
 }
 
 /* TESTED
 */
-function drawSquare(canvasCtx, squareSize, x, y, color1, color2) {
-	canvasCtx.fillStyle = color1;
+function drawSquare(canvasCtx, squareSize, x, y, col1, col2) {
+	canvasCtx.fillStyle = col1;
 	canvasCtx.fillRect((x * squareSize), (y * squareSize), squareSize, squareSize);
-	canvasCtx.fillStyle = color2;
+	canvasCtx.fillStyle = col2;
 	canvasCtx.fillRect((x * squareSize) + 2, (y * squareSize) + 2, squareSize - 4, squareSize - 4);
 }
 
 /* TESTED
 */
-function Snake(startSegment) {
+function drawOverSquare(canvasCtx, squareSize, x, y) {
+	canvasCtx.fillStyle = 'white';
+	canvasCtx.fillRect((x * squareSize), (y * squareSize), squareSize, squareSize);
+}
+
+/* TESTED
+*/
+function Snake(startSegment, startFood) {
+	this.color1 = 'red'
+	this.color2 = 'gold';
+	
 	this.snake = [startSegment];
+	this.oldTail = null;
+	this.food = startFood;
 	
 	this.length = function() { return this.snake.length; }
 	this.getHead = function() { return this.snake[0]; }
@@ -99,11 +80,15 @@ function Snake(startSegment) {
 	this.getHeadY = function() { return this.snake[0].getY(); }
 	this.getSnake = function() { return this.snake; }
 	
+	/* TESTED */
 	this.updateSnake = function(newHead, growSnake) {
 		this.snake.unshift(newHead);
-		if(!growSnake) { this.snake.pop(); }
+		if(!growSnake) { 
+			this.oldTail = this.snake.pop();
+		}
 	}
 	
+	/* TESTED */
 	this.intersectsSnake = function(x, y) {
 		for(var i = 0; i < this.snake.length; i++) {
 			var currSeg = this.snake[i];
@@ -115,9 +100,41 @@ function Snake(startSegment) {
 		return false;
 	}
 	
-	this.intersectHead = function(seg) {
-		var h= this.snake[0];
-		return (seg.getX() === h.getX() && seg.getY() === h.getY()) ? true : false;
+	/* TESTED */
+	this.headIntersectsFood = function() {
+		var s= this.snake[0];
+		return (this.food.getX() === s.getX() && this.food.getY() === s.getY());
+	}
+	
+	/* TESTED - only need to draw the new head and if necessary overdraw old tail
+	*/
+	this.drawSnake = function(ctx, gridElementLen) {
+		let s = this.snake[0];
+		drawSquare(ctx, gridElementLen, s.getX(), s.getY(), this.color1, this.color2);
+		
+		s = this.oldTail;
+		if(s !== null) {
+			drawOverSquare(ctx, gridElementLen, s.getX(), s.getY());
+		}
+	}
+	
+	this.drawFood = function(ctx, squareSize) {
+		drawSquare(ctx, squareSize, this.food.getX(), this.food.getY(), this.color1, this.color2);
+	}
+	
+	/* TESTED */
+	this.createNewFood = function(numDivisions) {
+		let x = getRand(numDivisions);
+		let y = getRand(numDivisions);
+		console.log('x = ' + x);
+		console.log('y = ' + y);
+		while(this.intersectsSnake(x, y)) {
+			x = getRand(numDivisions);
+			y = getRand(numDivisions);
+		}
+		this.food = new Coordinates(x, y);
+		console.log('food x = ' + this.food.getX());
+		console.log('food y = ' + this.food.getY());
 	}
 }
 
@@ -132,7 +149,7 @@ function outsideOfBorders(segment, numOfDivisions) {
 
 /* TESTED
 */
-function createNextSegment(keystroke, x, y, col1, col2) {
+function createNextSegment(keystroke, x, y) {
 	switch(keystroke) {
 		case 37: x += -1; //left
 				break;
@@ -144,21 +161,17 @@ function createNextSegment(keystroke, x, y, col1, col2) {
 				break;
 		default: return null;
 	}
-	return new SnakeSegment(x, y, col1, col2);
+	return new Coordinates(x, y);
 }
 
 /* TESTED
 */
-function SnakeSegment(x, y, color1, color2) {
+function Coordinates(x, y) {
 	this.x = x;
 	this.y = y;
-	this.color1 = color1;
-	this.color2 = color2;
 	
 	this.getX = function() { return this.x; }
 	this.getY = function() { return this.y; }
-	this.getColor1 = function() { return this.color1; }
-	this.getColor2 = function() { return this.color2; }
 }
 
 /* TESTED
@@ -168,24 +181,6 @@ function createBoard(dimension) {
 	var canvas = document.createElement('canvas');
 	canvas.width = canvas.height = dimension;
 	return canvas;
-}
-
-/* TESTED
-Draws a checkerboard grid on the canvas.
-*/
-function drawGameBoardGrid(canvasCtx, width, squareSize, color1, color2) {
-	var c = 0;
-	var drawColor = color1;
-	for(var i = 0; i < width; i += squareSize) {
-		drawColor = (c % 2 == 0) ? color1 : color2;
-		
-		for(var j = 0; j < width; j += squareSize) {
-			canvasCtx.fillStyle = drawColor;
-			canvasCtx.fillRect(i, j, squareSize, squareSize);
-			drawColor = (drawColor === color1) ? color2 : color1;
-		}
-		c++;
-	}
 }
 
 /* TESTED
